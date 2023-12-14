1618493571b0166cf25fffcad99ed352427cf623
sennewin <senne.winters@hotmail.nl>
2023-12-14 16:01:05 +0100

    GUI

--- GUI.py
+++ GUI.py
@@ -1,10 +1,19 @@
 from tkinter import *
+
+# Create root window
 root = Tk()
-root.title("Digital Twin GUI")
-root.attributes('-fullscreen', True)
+root.attributes('-fullscreen',True)
+
+# Create two frames at the top
+top_frame1 = Frame(root, bg="lightblue")
+top_frame2 = Frame(root, bg="lightgreen")
+bottom_frame = Frame(root, bg="lightpink")
+
+# Place frames on the grid
+top_frame1.grid(row=0, column=0, sticky="N")
+top_frame2.grid(row=0, column=1, sticky="S")
+bottom_frame.grid(row=1, column=0, columnspan=2, sticky="EW")
 
 
 
-exit_button = Button(root, text="Exit", command=root.destroy)
-exit_button.pack(pady=20)
 root.mainloop()
